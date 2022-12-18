e3e11d652354d08072f0fc8019c107c4367ce7df
balogunvictor <praisetheultimate@gmail.com>
2022-12-18 07:06:48 +0100

    added hero section to app

--- src/components/Getstarted.jsx
+++ src/components/Getstarted.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
+import styles from '../style';
+import { arrowUp } from '../assets';
 
-const Getstarted = () => {
-  return (
-    <div>Getstarted</div>
+const Getstarted = () =>  (
+    <div className={`${styles.flexCenter} w-[140px] h-[140px] roundedn-full bg-blue-gradient`}>
+    </div>
   )
-}
 
 export default Getstarted
